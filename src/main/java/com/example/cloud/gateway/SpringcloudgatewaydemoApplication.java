9dfac49be1a7e0fcd7f495aff3460d450e190745
vijay <vijay.srj@gmail.com>
2022-04-20 12:33:21 +0530

    changes

---
+++ src/main/java/com/example/cloud/gateway/SpringcloudgatewaydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudgatewaydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudgatewaydemoApplication.class, args);
+	}
+
+}
